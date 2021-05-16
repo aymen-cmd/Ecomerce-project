a1b140eec8ab75ea39fb0c2bd01ceb312edd5619
aymen benahcene <moundead34@gmail.com>
2021-05-16 18:11:04 +0200

    first commit

---
+++ database/factories/AdminTableFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Admin;
+use App\Model;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
+
+$factory->define(Admin::class, function (Faker $faker) {
+    return [
+        'name' => 'aymen benahcene',
+        'email' => 'aymen@gmail.com',
+        'email_verified_at' => now(),
+        'password' => Hash::make('admin'), // password
+        'remember_token' => Str::random(10),
+    ];
+});
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/activate/{code}', 'ActivationController@activateUserAccount')->name('user.activate');
+Route::get('/resend/{email}', 'ActivationController@resendActivationCode')->name('code.resend');
+Route::resource('products', 'ProductController');
+Route::get('products/category/{category}', 'HomeController@getProductByCategory')->name("category.products");
+Route::get('/cart', 'CartController@index')->name('cart.index');
+Route::post('/add/cart/{product}', 'CartController@addProductToCart')->name('add.cart');
+Route::delete('/remove/{product}/cart', 'CartController@removeProductFromCart')->name('remove.cart');
+Route::put('/update/{product}/cart', 'CartController@updateProductOnCart')->name('update.cart');
+Route::get('/handle-payment', 'PaypalPaymentController@handlePayment')->name('make.payment');
+Route::get('/cancel-payment', 'PaypalPaymentController@paymentCancel')->name('cancel.payment');
+Route::get('/payment-success', 'PaypalPaymentController@paymentSuccess')->name('success.payment');
+Route::get('/admin', 'AdminController@index')->name('admin.index');
+Route::get('/admin/login', 'AdminController@showAdminLoginForm')->name('admin.login');
+Route::post('/admin/login', 'AdminController@adminLogin')->name('admin.login');
+Route::get('/admin/logout', 'AdminController@adminLogout')->name('admin.logout');
+Route::get('/admin/products', 'AdminController@getProducts')->name('admin.products');
+Route::get('/admin/orders', 'AdminController@getOrders')->name('admin.orders');
+Route::resource('orders', 'OrderController');
+
